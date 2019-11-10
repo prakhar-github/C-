f11f56ac57db98b6c2a97fb77c33d7cbdb0cf64c
prakhar-github <lucky.chaturvedi96@gmail.com>
2019-11-10 23:30:17 +1000

    Add files via upload

---
+++ Airlines/Passenger.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using static System.Console;
+namespace Airlines
+{
+    public class Passenger
+    {
+        public Passenger()
+        {
+        }
+        private string[] lname, fname;
+        private int[] security_number, temp_arr;
+        private string departure_time = "19:00 PM", departure_date="01 Jun 2019", flight_no= "AIR745",times;
+        int gate = 8;
+
+        public string[] Lname
+        {
+            get { return lname; }
+            set { lname = value; }
+        }
+
+        public string[] Fname
+        {
+            get { return fname; }
+            set { fname = value; }
+        }
+
+
+        public string Departure_time
+        {
+            get { return departure_time;}
+
+        }
+
+        public string Departure_date
+        {
+            get { return departure_date;}
+        }
+
+        public int Gate
+        {
+            get { return gate;}
+        }
+        public string Flight_no
+        {
+            get { return flight_no;}
+        }
+
+        public int[] Security_number
+        {
+            get { return security_number; }
+        }
+
+        public int[] Temp_arr
+        {
+            get { return temp_arr; }
+        }
+
+        public string Time
+        {
+            get { return times;}
+            set { times = value;}
+
+        }
+
+
+        public Passenger(int arraysize) //constructor to insitalise array
+        {
+            this.fname = new string[arraysize];
+            this.lname = new string[arraysize];
+
+            this.security_number = new int[arraysize];
+            this.temp_arr = new int[arraysize];
+
+
+        }
+
+        public void empty_seats() //fucntion to check empty seats
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                if (this.fname[i] == null)
+                {
+
+                    WriteLine("seat {0} ", i + 1);
+                }
+
+
+
+            }
+        }
+
+        public int security_no(int min, int max) //random function to generate random numbers 
+        {
+            Random rnd = new Random();
+
+            return rnd.Next(min, max);
+
+        }
+
+        public void passenger_list() //function to generate passenger list
+        {
+            int j = 0;
+            for (int i =0; i<40; i++)
+                if(this.fname[i]!=null)
+                {
+                    WriteLine("Passenger list is displayed below");
+                    WriteLine("Boarding pass {0}", j + 1);
+                    WriteLine("First name : {0}", this.fname[i]);
+                    WriteLine("Last name : {0}", this.lname[i]);
+                    WriteLine("Seat number: {0}", i+1);
+                    WriteLine("Security number: {0}", this.security_number[i]);
+                    WriteLine();
+                    WriteLine("*************--------------------*************");
+                    j++;
+                }
+
+        }
+
+        public string truncate(string mystring) // function to truncate name except first five letters
+        {
+            if (mystring.Length > 5)
+                mystring = mystring.Substring(0, 5);
+            return mystring;
+        }
+
+
+        public void boarding_pass() // Function to issue boarding pass to all the passengers 
+        {
+            int k = 0;
+            for (int i =0; i<40; i++)
+            {
+
+                if(this.fname[i]!=null)
+                {
+                    WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                    WriteLine("\t\t\t\t\t\t\tBoarding pass");
+                    Write("Name: {0,4} {1,4} {2,2}", this.fname[i], this.lname[i], "|");
+                    Write("Seat: {0,4} {1,2}", i + 1, "|");
+                    Write("Departure Time: {0,4} {1,2}", this.departure_time, "|");
+                    Write("Departure Date: {0,4} {1,2}", this.departure_date, "|");
+                    Write("Security: {0,4} {1,2}", this.security_number[i], "|");
+                    Write("Flight: {0,4} {1,2}", this.flight_no, "|");
+                    Write("TIme: {0,6} {1,2}", this.times, "|");
+                    Write("Gate: {0,4}", this.gate);
+                    WriteLine();
+
+                    k++;
+
+                }
+                else if(k==0 && i==39)
+                {
+                    WriteLine("CANNOT ISSUE A BOARDING PASS! ALL SEATS ARE EMPTY.");
+                }
+               
+            }
+        }
+        public int Int_check(string input) // function to check if the input is integer or not
+        {
+            int number = 0;
+            bool valid = false;
+            while(valid == false)
+            {
+
+                if (int.TryParse(input, out number))
+                {
+                    valid = true;
+
+                }
+                else
+                {
+                    WriteLine("Wrong input, please enter a valid number");
+                    input = ReadLine();
+
+                }
+
+            }
+            return number;
+
+        }
+        public int seat_check(string[] booked, int seat) // function to check if the seat entered is empty or not
+        {
+            bool value = false;
+            seat = seat - 1;
+            while (value == false) 
+            { 
+            for (int i = 0; i < booked.Length; i++)
+            {
+                    if (booked[i] == null && seat == i)
+                    value = true;
+
+            }
+                if (value == false)
+                {
+                    WriteLine("Seat has already been booked, please select an empty seat");
+                    string resp = ReadLine();
+
+                    //int.TryParse(ReadLine(), out seat);
+                    seat = Int_check(resp);
+                    seat = seat - 1;
+                }
+        }
+            return seat + 1;
+        }
+        public string time()    // function to get time
+        {
+            DateTime now = DateTime.Now;
+            string times = Convert.ToString(now);
+            return times;
+
+
+        }
+
+
+    }
+    }
+
---
+++ Airlines/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using static System.Console;
+namespace Airlines
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string mystring, res, cusseat;
+            int response, end= 0, custres, custseat=0,number,airresp,booked_seats =0;
+            Passenger pass = new Passenger(40); // creating pass object for passenger class and calling a constructor
+            while(end!=999)// While loop for continuosly accepting passengers
+            {
+                bool valid = false;
+                if (booked_seats==40) // Notify Airline staff if the plane is at maximum capacity
+                {
+                    WriteLine("All the seats have been taken, Airplane is at maximum capacity!");
+                }
+                WriteLine("Do you want a boarding pass Y or press 999 to exit");
+                res = ReadLine();
+                if (res == "999")
+                    break;
+                else if (res == "Y" || res == "y")
+                {
+                    WriteLine("Press 1 for Airline Staff member, 2 for Passenger");
+                    // int.TryParse(ReadLine(), out response);
+                    res = ReadLine();
+                    response = pass.Int_check(res);
+                    switch (response)
+                    {
+                        case 1:
+                            Clear();
+                            WriteLine("You are selected as Airline Staff member");
+                            WriteLine("Press 1 to see available seats\nPress 2 to see passenger list\nPress 3 to issue boarding pass");
+                            res = ReadLine();
+                            airresp = pass.Int_check(res); // check if the number is integer or not
+                            //int.TryParse(ReadLine(), out airresp);
+                            switch (airresp)
+                            {
+                                case 1:
+                                    if (booked_seats <= 40)
+                                    {
+                                        WriteLine("Available seats are :");
+                                        pass.empty_seats();
+                                    }
+                                    else 
+                                    WriteLine("All seats are booked, no empty seats left");
+                                    break;
+                                case 2:
+                                    if (booked_seats == 0)
+                                        WriteLine("No passengner has booked a seat yet");
+                                    else
+                                    pass.passenger_list();
+                                    break;
+                                case 3:
+                                    if (booked_seats == 0)
+                                        WriteLine("No passengner has booked a seat yet, cannot issue a boarding pass!");
+                                    else
+                                    pass.boarding_pass();
+                                    break;
+
+
+                            }
+                            break;
+                        case 2:
+                            Clear();
+                            WriteLine("You are selected as a passanger");
+                            WriteLine("Press 1 to check empty seats, 2 to exit ");
+                            res = ReadLine();
+                            //int.TryParse(ReadLine(), out custres);
+                            custres = pass.Int_check(res);
+                            switch (custres)
+                            {
+                                case 1:
+                                    if (booked_seats == 40)
+                                        WriteLine("No empty seats left");
+                                    else
+                                    {
+                                        WriteLine("Empty Seats are displayed below");
+                                        pass.empty_seats();
+                                        while (valid == false)  // Loop to check if number of seats required to book isn't greater than available seats
+                                        { 
+                                        WriteLine("How mnay seats do you want to book");
+                                        cusseat = ReadLine();
+                                        custseat = pass.Int_check(cusseat);
+                                        int q =(custseat + booked_seats); 
+                                        if ((40-q) < 0)         // if block to compare total seats available and customer's required number of seats
+                                        {
+                                            WriteLine("Only {0} seats are left, can't book {1} seats", 40 - booked_seats, cusseat);
+                                        }
+                                        else
+                                            valid = true;
+                                    }
+                                        booked_seats = booked_seats + custseat; //increment total number of seats
+                                    //int.TryParse(ReadLine(), out custseat);
+                                    for (int i = 0; i < custseat; i++)
+                                    {
+                                        pass.Time = pass.time();// Time function to stamp system time on the boarding pass
+                                        WriteLine("select seat {0}", i + 1);
+                                        cusseat = ReadLine();
+                                        //int.TryParse(ReadLine(), out number);
+                                        number = pass.Int_check(cusseat);
+                                        number = pass.seat_check(pass.Fname,number);
+                                        WriteLine("Enter the First name");
+                                        mystring = ReadLine();
+                                        pass.Fname[number - 1] = pass.truncate(mystring);
+                                        WriteLine("Enter the last name");
+                                        mystring = ReadLine();
+                                        pass.Lname[number - 1] = pass.truncate(mystring);
+                                        pass.Security_number[number - 1] = pass.security_no(30000, 999999);
+                                        pass.Temp_arr[i] = number - 1; // stroing indexes of booked seats in Temp array to issue boarding pass after booking
+                                    }
+                                    Clear();
+                                        for (int i = 0; i < custseat; i++) // issue boarding pass to the passengers.
+                                        {
+                                            WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                                            WriteLine("\t\t\t\t\t\t\tBoarding pass {0,15}", i + 1);
+                                            int j = pass.Temp_arr[i];
+                                            Write("Name: {0,4} {1,4} {2,2}", pass.Fname[j], pass.Lname[j], "|");
+                                            Write("Seat: {0,4} {1,2}", j + 1, "|");
+                                            Write("Departure Time: {0,4} {1,2}", pass.Departure_time, "|");
+                                            Write("Departure Date: {0,4} {1,2}",pass.Departure_date,"|");
+                                            Write("Security: {0,4} {1,2}", pass.Security_number[j], "|");
+                                            Write("Flight: {0,4} {1,2}", pass.Flight_no, "|");
+                                            Write("TIme: {0,6} {1,2}", pass.Time, "|");
+                                            Write("Gate: {0,4}", pass.Gate);
+                                            WriteLine();
+
+                                    }
+                                    }
+
+                                    break;
+
+                                case 2:
+                                    break;
+
+                            }
+                            break;
+
+                    }
+                }
+
+
+
+                WriteLine();
+
+            }
+
+            ReadKey();
+
+        }// End of Main function
+    }// End Of Class
+}// End Of Namespace
